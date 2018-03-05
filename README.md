1a07535defa21e5a893cd44f9666a994f7481753
Gene Blanchard <gblanchard4@gmail.com>
2018-03-05 08:42:27 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# pi_seeds
-Raspberry Pi project for my seed starter
+## PiSeeds
+
+Tracking stats on my seed started using a raspberry pi and node.js
---
+++ server.js
@@ -0,0 +1,39 @@
+var sensor = require('ds18x20');
+var moment = require('moment');
+var FirebaseClient = require('firebase-client');
+var intervalTime = process.env.INTERVAL_TIME || 600;
+var firebaseURL = process.env.FIREBASE_URL;
+var deviceName = process.env.RESIN_DEVICE_UUID;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+var firebase = new FirebaseClient({url: firebaseURL});
+
+// reply to request with "Hello World!"
+app.get('/', function (req, res) {
+  res.render('index', {temp: null});
+});
+
+app.get('/temperature', function(req, res){
+  res.send(get_temperature)
+});
+
+sensor.list(function (err, listOfDeviceIds) {
+    console.log('Available Sensors: '+listOfDeviceIds);
+});
+
+function get_temperature() {
+  //reads all temp sensors
+  sensor.getAll(function (err, tempObj) {
+    console.log(tempObj);
+  });
+}
+
+//start a server on port 80 and log its start to our console
+app.listen(80, function () {
+  console.log('app running on port 80!')
+})
